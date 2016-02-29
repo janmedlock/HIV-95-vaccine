17e58475306635bb6bc489cc23b18650525fcd7e
Jan Medlock <jan.medlock@oregonstate.edu>
2016-02-28 16:03:24 -0800

    Use log model.

--- Codes/model/simulation.py
+++ Codes/model/simulation.py
@@ -9,7 +9,7 @@ from . import proportions
 t_end = 10
 
 
-def ODE_RHS(variables, t, parameters, target_funcs):
+def ODEs(state, t, parameters, target_funcs):
     # S is susceptible.
     # A is acute infection.
     # U is undiagnosed.
@@ -17,12 +17,12 @@ def ODE_RHS(variables, t, parameters, target_funcs):
     # T is treated but not viral suppressed.
     # V is viral suppressed.
     # W is AIDS.
-    S, A, U, D, T, V, W = variables
+    S, A, U, D, T, V, W = state
 
     # Total sexually active population.
     N = S + A + U + D + T + V
 
-    controls = control_rates.get_control_rates(t, variables, target_funcs)
+    controls = control_rates.get_control_rates(t, state, target_funcs)
 
     force_of_infection = (
         parameters.transmission_rate_acute * A
@@ -66,16 +66,92 @@ def ODE_RHS(variables, t, parameters, target_funcs):
     return (dS, dA, dU, dD, dT, dV, dW)
 
 
-def solve(target_values, parameters):
+def ODEs_log(state_log, t, parameters, target_funcs):
+    state = numpy.exp(state_log)
+    # S is susceptible.
+    # A is acute infection.
+    # U is undiagnosed.
+    # D is diagnosed but not treated.
+    # T is treated but not viral suppressed.
+    # V is viral suppressed.
+    # W is AIDS.
+    S, A, U, D, T, V, W = numpy.exp(state_log)
+    S_log, A_log, U_log, D_log, T_log, V_log, W_log = state_log
+
+    # Total sexually active population.
+    N = S + A + U + D + T + V
+
+    controls = control_rates.get_control_rates(t, state, target_funcs)
+
+    force_of_infection = (
+        parameters.transmission_rate_acute * A
+        + parameters.transmission_rate_unsuppressed * (U + D + T)
+        + parameters.transmission_rate_suppressed * V) / N
+
+    dS_log = (parameters.birth_rate * N * numpy.exp(- S_log)
+              - force_of_infection
+              - parameters.death_rate)
+
+    dA_log = (force_of_infection * numpy.exp(S_log - A_log)
+              - parameters.progression_rate_acute
+              - parameters.death_rate)
+
+    dU_log = (parameters.progression_rate_acute * numpy.exp(A_log - U_log)
+              - controls[0]
+              - parameters.death_rate
+              - parameters.progression_rate_unsuppressed)
+
+    dD_log = (controls[0] * numpy.exp(U_log - D_log)
+              + controls[2] * (numpy.exp(T_log - D_log)
+                               + numpy.exp(V_log - D_log))
+              - controls[1]
+              - parameters.death_rate
+              - parameters.progression_rate_unsuppressed)
+
+    dT_log = (controls[1] * numpy.exp(D_log - T_log)
+              - controls[2]
+              - parameters.suppression_rate
+              - parameters.death_rate
+              - parameters.progression_rate_unsuppressed)
+
+    dV_log = (parameters.suppression_rate * numpy.exp(T_log - V_log)
+              - controls[2]
+              - parameters.death_rate
+              - parameters.progression_rate_suppressed)
+
+    dW_log = (
+        parameters.progression_rate_unsuppressed * (numpy.exp(U_log - W_log)
+                                                    + numpy.exp(D_log - W_log)
+                                                    + numpy.exp(T_log - W_log))
+        + parameters.progression_rate_suppressed * numpy.exp(V_log - W_log)
+        - parameters.death_rate_AIDS)
+
+    return (dS_log, dA_log, dU_log, dD_log, dT_log, dV_log, dW_log)
+
+
+def solve(target_values, parameters, use_log = True):
     target_funcs = target_functions.get_target_funcs(target_values, parameters)
 
     t = numpy.linspace(0, t_end, 1001)
 
-    state = integrate.odeint(ODE_RHS,
-                             parameters.initial_conditions.copy(),
-                             t,
-                             args = (parameters, target_funcs),
-                             mxstep = 1000)
+    if use_log:
+        # Take log, but map 0 to e^-20.
+        Y0 = numpy.ma.log(parameters.initial_conditions).filled(-20)
+        fcn = ODEs_log
+    else:
+        Y0 = parameters.initial_conditions.copy()
+        fcn = ODEs
+        
+    Y = integrate.odeint(fcn,
+                         Y0,
+                         t,
+                         args = (parameters, target_funcs),
+                         mxstep = 1000)
+
+    if use_log:
+        state = numpy.exp(Y)
+    else:
+        state = Y
 
     return (t, state, target_funcs)
 
---
+++ Codes/tests/logmodel.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import numpy
+import time
+
+import sys
+sys.path.append('..')
+
+import model
+
+
+country = 'Nigeria'
+
+parameters = model.Parameters(country)
+
+time0 = time.time()
+t, state, target_funcs = model.solve('909090', parameters, use_log = False)
+time1 = time.time()
+print('Non-log model took {} sec.'.format(time1 - time0))
+
+time0_ = time.time()
+t_, state_, target_funcs_ = model.solve('909090', parameters, use_log = True)
+time1_ = time.time()
+print('Log model took {} sec.'.format(time1_ - time0_))
+
+
+# Ignore differences in the initial conditions.
+maxabserr = numpy.abs(state - state_)[1 : ].max()
+maxrelerr = (numpy.abs(state - state_) / state)[1 : ].max()
+
+print('Max absolute error = {}'.format(maxabserr))
+print('Max relative error = {}'.format(maxrelerr))
