e0237257c3c84d2c10314ce27859281ceafcb15f
Jan Medlock <jan.medlock@oregonstate.edu>
2016-03-03 11:43:21 -0800

    Split out testing code.

---
+++ Codes/mapplot/test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import numpy
+
+import mapplot
+
+
+def main():
+    countries = ('Canada', 'United States of America', 'Mexico',
+                 'Guatemala', 'Honduras', 'El Salvador', 'Belize',
+                 'Costa Rica', 'Nicaragua', 'Panama')
+
+    data = numpy.random.uniform(size = (len(countries), 5))
+
+    m = mapplot.Basemap()
+
+    m.choropleth(countries, data[:, 0], zorder = 1)
+
+    m.scatter(countries, c = data[:, 1], s = 40 * data[:, 2],
+              zorder = 2)
+
+    data_normalized = data[:, 0 : -1] / data[:, 0 : -1].sum(1)[:, numpy.newaxis]
+    m.pies(countries, data_normalized,
+           s = 40 * data[-1],
+           wedgeprops = dict(linewidth = 0, zorder = 3))
+    
+    mapplot.pyplot.show()
+
+
+if __name__ == '__main__':
+    main()
