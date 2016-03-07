1aa8b24e1d608f4ce90d760a6ea2a25f09c48dcf
Jan Medlock <jan.medlock@oregonstate.edu>
2016-03-07 14:33:15 -0800

    Add marginal cost of effort and put it with total cost inside a class.

--- Codes/model/CE_stats.py
+++ Codes/model/CE_stats.py
@@ -10,30 +10,30 @@ from . import simulation
 from . import targets
 
 
-def relative_cost_of_effort(p, breakpoint = 0.8):
+class RelativeCostOfEffort:
     r'''
-    Total cost of effort for level `p`,
+    The marginal cost of effort for level `p`,
     designed to follow the 80–20 rule.
 
-    This total cost is the integral
-
-    .. math::
-      F(p) = \int_0^p f(p') \mathrm{d}p'
-
-    of the marginal cost
+    The marginal cost is
 
     .. math::
        f(p) =
        \begin{cases}
        1 & \text{if $p \leq b$},
        \\
-       1 + m (p - b) & \text{if $p \geq b$}.
+       1 + m (p - b) & \text{if $p \geq b$},
        \end{cases}
+
+    and the total cost is
+
+    .. math::
+      F(p) = \int_0^p f(p') \mathrm{d}p'.
 
     This gives proportion :math:`b` of total cost in last :math:`(1 - b)`,
 
     .. math::
-       \frac{F(1) - F(b)}{F(1)} = b.
+       \frac{F(1) - F(b)}{F(1)} = b,
        :label: eightytwenty
 
     Particularly, with :math:`b = 0.8`, it gives the 80–20 rule,
@@ -48,24 +48,48 @@ def relative_cost_of_effort(p, breakpoint = 0.8):
 
     :Testing:
 
-    Check that this function satisfies :eq:`eightytwenty`:
+    Check that these functions satisfy :eq:`eightytwenty`:
 
     .. doctest::
 
-       >>> import numpy
-       >>> import model.CE_stats
-       >>> F = model.CE_stats.relative_cost_of_effort
-       >>> for b in numpy.arange(0.5, 1, 0.1):
-       ...    assert numpy.isclose((F(1, b) - F(b, b)) / F(1, b), b)
+       >>> from numpy import arange, isclose
+       >>> from scipy.integrate import quad
+       >>> from model.CE_stats import RelativeCostOfEffort
+       >>> F = RelativeCostOfEffort.total_cost
+       >>> def G(p, b):
+       ...     return quad(RelativeCostOfEffort.marginal_cost,
+       ...                 0, p, args = (b, ), points = (b, ))[0]
+       >>> for b in arange(0.5, 1, 0.1):
+       ...    assert isclose((F(1, b) - F(b, b)) / F(1, b), b)
+       ...    assert isclose((G(1, b) - G(b, b)) / G(1, b), b)
     '''
-    assert (0.5 <= breakpoint < 1), 'Bad breakpoint = {}.'.format(breakpoint)
-    # assert numpy.all((0 <= p) & (p <= 1)), 'Bad p = {}.'.format(p)
-    
-    slope = 2 * (2 * breakpoint  - 1) / (1 - breakpoint) ** 3
+    @staticmethod
+    def marginal_cost(p, breakpoint = 0.8):
+        assert (0.5 <= breakpoint < 1), 'Bad breakpoint = {}.'.format(
+            breakpoint)
+        # assert numpy.all((0 <= p) & (p <= 1)), 'Bad p = {}.'.format(p)
 
-    return numpy.where(
-        p < breakpoint, p,
-        p + slope / 2 * (p - breakpoint) ** 2)
+        slope = 2 * (2 * breakpoint  - 1) / (1 - breakpoint) ** 3
+
+        p = numpy.asarray(p)
+
+        return numpy.where(
+            p < breakpoint, 1,
+            1 + slope * (p - breakpoint))
+
+    @staticmethod
+    def total_cost(p, breakpoint = 0.8):
+        assert (0.5 <= breakpoint < 1), 'Bad breakpoint = {}.'.format(
+            breakpoint)
+        # assert numpy.all((0 <= p) & (p <= 1)), 'Bad p = {}.'.format(p)
+
+        slope = 2 * (2 * breakpoint  - 1) / (1 - breakpoint) ** 3
+
+        p = numpy.asarray(p)
+
+        return numpy.where(
+            p < breakpoint, p,
+            p + slope / 2 * (p - breakpoint) ** 2)
 
 
 def get_CE_stats(t, state, targs, parameters):
@@ -84,7 +108,7 @@ def get_CE_stats(t, state, targs, parameters):
             # multiplied by
             # the relative cost of effort (increasing marginal costs)
             # for diagnosis (target_values[0]),
-            * relative_cost_of_effort(target_values[..., 0])
+            * RelativeCostOfEffort.total_cost(target_values[..., 0])
             # the level of diagnosis control (controls[0]),
             * controls[..., 0]
             # and the number of Susceptible, Acute, & Undiagnosed
@@ -104,7 +128,7 @@ def get_CE_stats(t, state, targs, parameters):
             # multiplied by
             # the relative cost of effort (increasing marginal costs)
             # for treatment (target_values[1]),
-            * relative_cost_of_effort(target_values[..., 1])
+            * RelativeCostOfEffort.total_cost(target_values[..., 1])
             # and the number of people Treated & Suppressed
             # (state[[4, 5]]).
             * state[..., [4, 5]].sum(-1)
@@ -114,7 +138,7 @@ def get_CE_stats(t, state, targs, parameters):
             # multiplied by
             # the relative cost of effort (increasing marginal costs)
             # for nonadherance (target_values[2]),
-            * relative_cost_of_effort(target_values[..., 2])
+            * RelativeCostOfEffort.total_cost(target_values[..., 2])
             # and the number of people Treated and Suppressed
             # (state[[4, 5]]).
             * state[..., [4, 5]].sum(-1)
