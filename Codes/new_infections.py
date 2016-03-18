ac5b230f26d82459cd52e32f808c5cade32aa586
Jan Medlock <jan.medlock@oregonstate.edu>
2016-03-18 10:52:37 -0700

    New scripts to plot prevalence and new infections.

---
+++ Codes/new_infections.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+'''
+Make an animated map of the prevalence at different times.
+'''
+
+import pickle
+import warnings
+
+from matplotlib import pyplot
+from matplotlib import ticker
+
+# Silence warnings from matplotlib trigged by seaborn.
+warnings.filterwarnings(
+    'ignore',
+    module = 'matplotlib',
+    message = ('axes.color_cycle is deprecated '
+               'and replaced with axes.prop_cycle; '
+               'please use the latter.'))
+import seaborn
+
+
+countries = ('United States of America',
+             'South Africa',
+             'Rwanda',
+             'Uganda',
+             'India',
+             'Haiti')
+
+
+def _main():
+    results = pickle.load(open('909090.pkl', 'rb'))
+
+    colors = seaborn.color_palette('husl', len(countries))
+
+    fig, ax = pyplot.subplots()
+    for (c, x) in zip(countries, colors):
+        try:
+            r = results[c]
+        except KeyError:
+            pass
+        else:
+            ax.plot(r.solution.t + 2015,
+                    r.solution.new_infections / 1000,
+                    color = x, linestyle = 'solid',
+                    label = '{}, 90–90–90'.format(c))
+            ax.plot(r.solution_base.t + 2015,
+                    r.solution_base.new_infections / 1000,
+                    color = x, linestyle = 'dotted',
+                    label = '{}, status quo'.format(c))
+
+    ax.set_xlim(r.solution.t[0] + 2015,
+                r.solution.t[-1] + 2015)
+
+    ax.set_xlabel('Year')
+    ax.set_ylabel('New Infections (1000s)')
+    ax.legend(loc = 'upper left')
+
+    fig.savefig('new_infections.pdf')
+
+    pyplot.show()
+
+
+if __name__ == '__main__':
+    _main()
---
+++ Codes/prevalence.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+'''
+Make an animated map of the prevalence at different times.
+'''
+
+import pickle
+import warnings
+
+from matplotlib import pyplot
+from matplotlib import ticker
+
+# Silence warnings from matplotlib trigged by seaborn.
+warnings.filterwarnings(
+    'ignore',
+    module = 'matplotlib',
+    message = ('axes.color_cycle is deprecated '
+               'and replaced with axes.prop_cycle; '
+               'please use the latter.'))
+import seaborn
+
+
+countries = ('United States of America',
+             'South Africa',
+             'Rwanda',
+             'Uganda',
+             'India',
+             'Haiti')
+
+
+def _main():
+    results = pickle.load(open('909090.pkl', 'rb'))
+
+    colors = seaborn.color_palette('husl', len(countries))
+
+    fig, ax = pyplot.subplots()
+    for (c, x) in zip(countries, colors):
+        try:
+            r = results[c]
+        except KeyError:
+            pass
+        else:
+            ax.semilogy(r.solution.t + 2015,
+                        100 * r.solution.prevalence,
+                        color = x, linestyle = 'solid',
+                        label = '{}, 90–90–90'.format(c))
+            ax.semilogy(r.solution_base.t + 2015,
+                        100 * r.solution_base.prevalence,
+                        color = x, linestyle = 'dotted',
+                        label = '{}, status quo'.format(c))
+
+    ax.set_xlim(r.solution.t[0] + 2015,
+                r.solution.t[-1] + 2015)
+    ax.set_ylim(0.1, 100)
+
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Prevalence')
+    ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%g%%'))
+    ax.legend(loc = 'upper right')
+
+    fig.savefig('prevalence.pdf')
+
+    pyplot.show()
+
+
+if __name__ == '__main__':
+    _main()
