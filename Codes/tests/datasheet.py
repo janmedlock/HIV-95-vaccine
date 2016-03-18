5a824bb73a870176c2f2b043818eb7ba1a5edd83
Jan Medlock <jan.medlock@oregonstate.edu>
2016-03-18 15:34:41 -0700

    Updated datasheet.

--- Codes/tests/datasheet.py
+++ Codes/tests/datasheet.py
@@ -9,12 +9,28 @@ sys.path.append('..')
 import model
 
 
-def _main():
-    country = 'Nigeria'
-
+def test_one(country):
+    '''
+    Test loading one country's data from the datasheet.
+    '''
     parameters = model.Parameters(country)
 
     print(parameters)
+
+
+def test_all():
+    '''
+    Test loading all data from the datasheet.
+    '''
+    for country in model.get_country_list():
+        print(country)
+        model.Parameters(country)
+
+
+def _main():
+    test_one('Nigeria')
+    
+    test_all()
 
 
 if __name__ == '__main__':
