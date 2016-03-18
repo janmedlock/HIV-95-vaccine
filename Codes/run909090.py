4141c8b8555060234f5be27da5f8f7d959934c6c
Jan Medlock <jan.medlock@oregonstate.edu>
2016-03-18 12:29:45 -0700

    Whitespace.

--- Codes/run909090.py
+++ Codes/run909090.py
@@ -24,6 +24,7 @@ def _main():
         results = parallel(
             joblib.delayed(_helper)(country)
             for country in model.get_country_list())
+
     pickle.dump(dict(results), open('909090.pkl', 'wb'))
 
 
