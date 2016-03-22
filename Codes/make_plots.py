e5ce4f819ac0aa3a8b4b5f51c1e99e83c99e1600
Jan Medlock <jan.medlock@oregonstate.edu>
2016-03-22 09:58:54 +0100

    Make global plots.

--- Codes/make_plots.py
+++ Codes/make_plots.py
@@ -4,13 +4,13 @@ Plot new infections.
 '''
 
 import collections
-import collections.abc
 import itertools
 import pickle
 import warnings
 
 from matplotlib import pyplot
 from matplotlib import ticker
+import numpy
 
 # Silence warnings from matplotlib trigged by seaborn.
 warnings.filterwarnings(
@@ -20,6 +20,24 @@ warnings.filterwarnings(
                'and replaced with axes.prop_cycle; '
                'please use the latter.'))
 import seaborn
+
+import model.container
+
+
+class Global(model.container.Container):
+    '''
+    Dummy class to hold global results.
+    '''
+
+    _keys = ('AIDS', 'alive', 'dead', 'infected', 'new_infections')
+
+    def __init__(self, t):
+        for k in self.keys():
+            setattr(self, k, numpy.zeros_like(t))
+
+    @property
+    def prevalence(self):
+        return self.infected / self.alive
 
 
 def getlabel(k):
@@ -38,23 +56,29 @@ def getlabel(k):
     return label
 
 
-def baseplot(ax, t, data, ylabel,
+def baseplot(ax, t, data, ylabel, scale = 1,
              legend = True, xlabel = True, title = None,
              percent = False):
     linestyles = ('dotted', 'solid')
-    # colors = seaborn.color_palette('husl', len(data) // len(linestyles))
     colors = seaborn.color_palette('Set2', len(data) // len(linestyles))
     styles = list(itertools.product(linestyles, colors))
     for (x, style) in zip(data.items(), styles):
         k, v = x
         ls, c = style
-        ax.plot(t + 2015, v,
+        ax.plot(t + 2015, v / scale,
                 linestyle = ls,
                 color = c,
                 label = getlabel(k))
     ax.set_xlim(t[0] + 2015, t[-1] + 2015)
     if xlabel:
         ax.set_xlabel('Year')
+    if scale > 1:
+        if scale == 1e3:
+            ylabel += ' (1000s)'
+        elif scale == 1e6:
+            ylabel += ' (M)'
+        else:
+            ylabel += ' ({:g})'.format(scale)
     ax.set_ylabel(ylabel)
     if percent:
         ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%g%%'))
@@ -67,32 +91,32 @@ def baseplot(ax, t, data, ylabel,
 def new_infections(ax, t, data, **kwargs):
     data_ = collections.OrderedDict()
     for (k, v) in data.items():
-        data_[k] = v.new_infections / 1000
-    ylabel = 'New Infections (1000s)'
+        data_[k] = v.new_infections
+    ylabel = 'New Infections'
     baseplot(ax, t, data_, ylabel, **kwargs)
 
 
 def dead(ax, t, data, **kwargs):
     data_ = collections.OrderedDict()
     for (k, v) in data.items():
-        data_[k] = v.dead / 1000
-    ylabel = 'AIDS Deaths (1000s)'
+        data_[k] = v.dead
+    ylabel = 'AIDS Deaths'
     baseplot(ax, t, data_, ylabel, **kwargs)
 
 
 def infected(ax, t, data, **kwargs):
     data_ = collections.OrderedDict()
     for (k, v) in data.items():
-        data_[k] = v.infected / 1000
-    ylabel = 'People Living with HIV (1000s)'
+        data_[k] = v.infected
+    ylabel = 'People Living with HIV'
     baseplot(ax, t, data_, ylabel, **kwargs)
 
 
 def AIDS(ax, t, data, **kwargs):
     data_ = collections.OrderedDict()
     for (k, v) in data.items():
-        data_[k] = v.AIDS / 1000
-    ylabel = 'People with AIDS (1000s)'
+        data_[k] = v.AIDS
+    ylabel = 'People with AIDS'
     baseplot(ax, t, data_, ylabel, **kwargs)
 
 
@@ -104,31 +128,49 @@ def prevalence(ax, t, data, **kwargs):
     baseplot(ax, t, data_, ylabel, percent = True, **kwargs)
 
 
-countries = ('United States of America',
-             'South Africa',
-             'Rwanda',
-             'Uganda',
-             'India',
-             'Haiti')
-
 def _main():
+    countries_to_plot = ('United States of America',
+                         'South Africa',
+                         'Rwanda',
+                         'Uganda',
+                         'India',
+                         'Haiti',
+                         'Global')
+
     results = pickle.load(open('909090.pkl', 'rb'))
 
-    # countries = list(results.keys())
+    countries = list(results.keys())
     levels = list(results[countries[0]].keys())
-
     t = results[countries[0]][levels[0]].t
-    for country in countries:
+    for country in countries_to_plot:
         fig, axes = pyplot.subplots(3, figsize = (11, 8.5), sharex = True)
-        new_infections(axes[0], t, results[country],
-                       title = country, xlabel = False)
-        infected(axes[1], t, results[country], xlabel = False,
-                 legend = False)
-        AIDS(axes[2], t, results[country], legend = False)
+        if country == 'Global':
+            data = collections.OrderedDict()
+            for l in levels:
+                data[l] = Global(t)
+            for c in countries:
+                for l in levels:
+                    for k in data[l].keys():
+                        setattr(data[l], k,
+                                getattr(data[l], k)
+                                + getattr(results[c][l], k))
+            scale = 1e6
+        else:
+            data = results[country]
+            scale = 1e3
+        title = country
+        if country == 'Global':
+            title += ' ({:d} countries)'.format(len(countries))
+        new_infections(axes[0], t, data, scale = scale,
+                       title = title, xlabel = False)
+        infected(axes[1], t, data, scale = scale,
+                 xlabel = False, legend = False)
+        AIDS(axes[2], t, data, scale = scale,
+             legend = False)
         fig.tight_layout()
         fig.savefig('{}.pdf'.format(country.replace(' ', '_')))
 
-    pyplot.show()
+    # pyplot.show()
 
 
 if __name__ == '__main__':
