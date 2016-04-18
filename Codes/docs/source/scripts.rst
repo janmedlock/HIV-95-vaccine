5ec16bb109470f8667a8f88c6370b50036e709fa
Jan Medlock <jan.medlock@oregonstate.edu>
2016-04-18 15:15:28 -0700

    Remove run909090.py

--- Codes/docs/source/scripts.rst
+++ Codes/docs/source/scripts.rst
@@ -13,10 +13,6 @@ map_initial_proportions
 -----------------------
 .. automodule:: map_initial_proportions
 
-map909090
----------
-.. automodule:: map909090
-
 drive_download
 --------------
 .. automodule:: drive_download
--- Codes/generate_samples.py
+++ Codes/generate_samples.py
@@ -7,10 +7,12 @@ import model
 
 samplesfile = 'samples.pkl'
 
-nsamples = 2
+nsamples = 1000
 
 if __name__ == '__main__':
     samples = model.Parameters.generate_samples(nsamples)
+
+    print('Generated {} samples.'.format(len(samples)))
 
     with open(samplesfile, 'wb') as fd:
         pickle.dump(samples, fd)
