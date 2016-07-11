669eb4f48a7f01cda6709fba5a3a6af6742cf9df
Jan Medlock <jan.medlock@oregonstate.edu>
2016-07-11 16:06:10 -0700

    Generalize plotting of effectiveness.

--- Codes/plots/effectiveness.py
+++ Codes/plots/effectiveness.py
@@ -23,23 +23,6 @@ sys.path.append('..')
 import model
 
 
-alpha = 0.1
-
-# Run each of these and each of these + vaccine.
-targets_baseline = [model.targets.StatusQuo(),
-                    model.targets.UNAIDS90(),
-                    model.targets.UNAIDS95()]
-targets = []
-for target in targets_baseline:
-    targets.extend([target,
-                    model.targets.Vaccine(treatment_targets = target)])
-
-# cp = seaborn.color_palette('colorblind')
-# colors = (cp[2], cp[0])
-# cp = seaborn.color_palette('Dark2')
-# colors = (cp[1], cp[0])
-colors = seaborn.color_palette('Paired', len(targets))
-
 country_label_replacements = {
     'United States of America': 'United States'
 }
@@ -53,7 +36,8 @@ def getlabel(target):
     return retval
 
 
-def plotcell(ax, country, attr,
+def plotcell(ax, country, targets, attr,
+             confidence_level = 0.9,
              country_label = None, attr_label = None, legend = False):
     scale = 1
     percent = False
@@ -81,12 +65,14 @@ def plotcell(ax, country, attr,
         t = model.results.data[(country, target, 't')]
         v = model.results.data[(country, target, attr)]
 
-        avg, CI = common.getstats(v, alpha = alpha)
-        ax.plot(t, avg / scale, color = colors[i], label = getlabel(target),
-                zorder = 2)
-        ax.fill_between(t, CI[0] / scale, CI[1] / scale,
-                        color = colors[i],
-                        alpha = 0.3)
+        avg, CI = common.getstats(v, alpha = 1 - confidence_level)
+        lines = ax.plot(t, avg / scale, label = getlabel(target),
+                        zorder = 2)
+        if confidence_level > 0:
+            color = lines[0].get_color()
+            ax.fill_between(t, CI[0] / scale, CI[1] / scale,
+                            color = color,
+                            alpha = 0.3)
 
     ax.set_xlim(t[0], t[-1])
     ax.grid(True, which = 'both', axis = 'both')
@@ -116,7 +102,10 @@ def plotcell(ax, country, attr,
         ax.legend(loc = 'upper left')
 
 
-def plot_selected():
+def plot_somecountries(targets, ncol = None, **kwargs):
+    if ncol is None:
+        ncol = len(targets)
+
     fig = pyplot.figure(figsize = (8.5, 11))
 
     # Legend in tiny bottom row
@@ -129,100 +118,160 @@ def plot_selected():
 
         plotcell(fig.add_subplot(gs[i, 0]),
                  country,
+                 targets,
                  'infected',
                  country_label = 'ylabel',
-                 attr_label = attr_label)
+                 attr_label = attr_label,
+                 **kwargs)
 
         plotcell(fig.add_subplot(gs[i, 1]),
                  country,
+                 targets,
                  'AIDS',
-                 attr_label = attr_label)
+                 attr_label = attr_label,
+                 **kwargs)
 
         plotcell(fig.add_subplot(gs[i, 2]),
                  country,
+                 targets,
                  'incidence_per_capita',
-                 attr_label = attr_label)
+                 attr_label = attr_label,
+                 **kwargs)
 
         plotcell(fig.add_subplot(gs[i, 3]),
                  country,
+                 targets,
                  'prevalence',
-                 attr_label = attr_label)
+                 attr_label = attr_label,
+                 **kwargs)
 
-    lines_ = [lines.Line2D([0], [0], color = colors[i])
-              for i in range(len(targets))]
-    axis = fig.add_subplot(gs[-1, :], axis_bgcolor = 'none')
-    axis.tick_params(labelbottom = False, labelleft = False)
-    axis.grid(False)
-    labels = list(map(getlabel, targets))
-    axis.legend(lines_, labels,
-                loc = 'center',
-                ncol = len(targets) // 2,
-                frameon = False,
-                fontsize = 'medium')
+    # Make legend at bottom.
+    axes = fig.add_subplot(gs[-1, :], axis_bgcolor = 'none')
+    axes.tick_params(labelbottom = False, labelleft = False)
+    axes.grid(False)
+    # Dummy invisible lines.
+    for target in targets:
+        axes.plot(0, 0, label = getlabel(target), visible = False)
+    legend = axes.legend(loc = 'center',
+                         ncol = ncol,
+                         frameon = False,
+                         fontsize = 'medium')
+    # Make legend lines visible.
+    for line in legend.get_lines():
+        line.set_visible(True)
 
     fig.tight_layout()
 
+    return fig
+
+
+def plot_somecountries_alltargets(targets,
+                                  confidence_level = 0,
+                                  **kwargs):
+    with seaborn.color_palette('Paired', len(targets)):
+        fig = plot_somecountries(targets,
+                                 ncol = len(targets) // 2,
+                                 confidence_level = confidence_level,
+                                 **kwargs)
     fig.savefig('{}.pdf'.format(common.get_filebase()))
     fig.savefig('{}.png'.format(common.get_filebase()))
+    return fig
 
 
-def plot_all():
+def plot_somecountries_pairedtargets(targets,
+                                     **kwargs):
+    cp = seaborn.color_palette('colorblind')
+    colors = [cp[2], cp[0], cp[3], cp[1], cp[4], cp[5]]
+    # cp = seaborn.color_palette('Dark2')
+    # colors = [cp[1], cp[0], cp[3], cp[2], cp[5], cp[4]]
+
+    filebase = common.get_filebase()
+    figs = []
+    for i in range(len(targets) // 2):
+        targets_ = targets[2 * i : 2 * i + 2]
+        with seaborn.color_palette(colors):
+            fig = plot_somecountries(targets_, **kwargs)
+        figs.append(fig)
+        filebase_suffix = str(targets_[0]).replace(' ', '_')
+        filebase_ = '{}_{}'.format(filebase, filebase_suffix)
+        fig.savefig('{}.pdf'.format(filebase))
+        fig.savefig('{}.png'.format(filebase))
+
+    return figs
+
+
+def plot_allcountries(targets, **kwargs):
     countries = ['Global'] + sorted(model.get_country_list())
 
     filename = '{}_all.pdf'.format(common.get_filebase())
     with backend_pdf.PdfPages(filename) as pdf:
         for (i, country) in enumerate(countries):
-            with model.results.Results(country) as results:
-                if country == 'United States of America':
-                    title = 'United States'
-                else:
-                    title = country
+            fig, axes = pyplot.subplots(4,
+                                        figsize = (11, 8.5),
+                                        sharex = True,
+                                        squeeze = True)
 
-                fig, axes = pyplot.subplots(4,
-                                            figsize = (11, 8.5),
-                                            sharex = True,
-                                            squeeze = True)
+            try:
+                plotcell(axes[0],
+                         country,
+                         targets,
+                         'infected',
+                         attr_label = 'ylabel',
+                         country_label = 'title',
+                         legend = True,
+                         **kwargs)
 
-                try:
-                    plotcell(axes[0],
-                             results.getfield('infected'),
-                             scale = 1e6,
-                             ylabel = 'People Living with HIV\n(M)',
-                             legend = True,
-                             title = title)
+                plotcell(axes[1],
+                         country,
+                         targets,
+                         'AIDS',
+                         attr_label = 'ylabel',
+                         **kwargs)
 
-                    plotcell(axes[1],
-                             results.getfield('AIDS'),
-                             scale = 1e3,
-                             ylabel = 'People with AIDS\n(1000s)',
-                             legend = False)
+                plotcell(axes[2],
+                         country,
+                         targets,
+                         'incidence_per_capita',
+                         attr_label = 'ylabel',
+                         **kwargs)
 
-                    plotcell(axes[2],
-                             results.getfield('incidence_per_capita'),
-                             scale = 1e-6,
-                             ylabel = 'HIV Incidence\n(per M people per y)',
-                             legend = False)
-
-                    plotcell(axes[3],
-                             results.getfield('prevalence'),
-                             percent = True,
-                             ylabel = 'HIV Prevelance\n',
-                             legend = False)
-                except FileNotFoundError:
-                    pass
-                else:
-                    fig.tight_layout()
-                    pdf.savefig(fig)
-                finally:
-                    pyplot.close(fig)
+                plotcell(axes[3],
+                         country,
+                         targets,
+                         'prevalence',
+                         attr_label = 'ylabel',
+                         **kwargs)
+            except FileNotFoundError:
+                pass
+            else:
+                fig.tight_layout()
+                pdf.savefig(fig)
+            finally:
+                pyplot.close(fig)
 
 
 if __name__ == '__main__':
+    # Each of these and each of these + vaccine.
+    targets_baseline = [model.targets.StatusQuo(),
+                        model.targets.UNAIDS90(),
+                        model.targets.UNAIDS95()]
+    targets = []
+    for target in targets_baseline:
+        targets.extend([target,
+                        model.targets.Vaccine(treatment_targets = target)])
+
+    confidence_level = 0.9
+
+    # Remove 'Global' for now.
     common.countries_to_plot = list(common.countries_to_plot)
     common.countries_to_plot.remove('Global')
 
-    plot_selected()
+    plot_somecountries_alltargets(targets)
 
-    # plot_all()
+    plot_somecountries_pairedtargets(targets,
+                                     confidence_level = confidence_level)
+
+    # plot_allcountries(targets,
+    #                   confidence_level = confidence_level)
 
     pyplot.show()
