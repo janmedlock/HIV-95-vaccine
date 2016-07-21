f9ab620c1969b924784980117e830b2b2ed71f40
Jan Medlock <jan.medlock@oregonstate.edu>
2016-07-21 10:35:24 -0700

    Updated drive_upload.

--- Codes/plots/drive_upload.py
+++ Codes/plots/drive_upload.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 '''
 Upload figures etc to Google Drive.
-
-.. todo:: Update me!
 '''
 
+import os.path
 import sys
 
 sys.path.append('../..')
@@ -12,19 +11,25 @@ import drive
 
 
 files = (
+    '../../Documents/Latex/diagram_standalone.pdf',
+    '../../Documents/Latex/diagram_standalone.png',
     'effectiveness_all.pdf',
     'effectiveness.pdf',
     'effectiveness.png',
-    'infections_averted_map.mp4',
     'infections_averted_map.pdf',
     'infections_averted_map.png',
-    'prevalence_map.mp4',
-    'table.csv'
+    'initial_prevalence_map.pdf',
+    'initial_prevalence_map.png',
+    'transmission_rate.pdf',
+    'transmission_rate.png',
+    'vaccine_sensitivity_Status_Quo.pdf',
+    'vaccine_sensitivity_Status_Quo.png',
+    'vaccine_sensitivity_95–95–95.pdf',
+    'vaccine_sensitivity_95–95–95.png'
 )
        
-
-# 'Effectiveness of 90-90-90 targets files' folder
-folderId = '0B_53qFSHU3XKLS1TQjFWVl9HZGs'
+# 'Effectiveness of HIV vaccine files' folder
+folderId = '0B_53qFSHU3XKNjc0d3lTV2s4cWM'
 
 
 if __name__ == '__main__':
