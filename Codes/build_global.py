f8f6c48606f31beb9dddfe7fc715efb8ba4b444b
Jan Medlock <jan.medlock@oregonstate.edu>
2016-08-03 14:22:50 -0700

    Minor tweaks.

--- Codes/build_global.py
+++ Codes/build_global.py
@@ -6,18 +6,13 @@ Update the Global results.
 import model
 
 
-# 90-90-90 isn't finished yet.
-# targets = model.targets.all_
-targets_baselines = [model.targets.StatusQuo(),
-                     model.targets.UNAIDS95()]
-targets = []
-for t in targets_baselines:
-    targets.extend([t, model.targets.Vaccine(treatment_targets = t)])
-
-
-if __name__ == '__main__':
-    for target in targets:
+def _main():
+    for target in model.targets.all_:
         if not model.results.samples.exists('Global', target):
             results = model.results.samples.Results('Global', target)
             results._load_data()
             model.results.samples.dump('Global', target, results)
+
+
+if __name__ == '__main__':
+    _main()
