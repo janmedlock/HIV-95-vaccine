2dff832d3087e09ee26ef57142ef25435c934f62
Jan Medlock <jan.medlock@oregonstate.edu>
2016-08-05 15:10:16 -0700

    Change path.

--- Codes/model/results/samples/h5.py
+++ Codes/model/results/samples/h5.py
@@ -9,6 +9,5 @@ from ... import h5
 
 
 def open_(mode = 'r'):
-    # return h5.open_file(os.path.join(common.resultsdir, 'samples.h5'),
-    #                     mode = mode)
-    return h5.open_file('/media/backup2/samples.h5', mode = mode)
+    return h5.open_file(os.path.join(common.resultsdir, 'samples.h5'),
+                        mode = mode)
