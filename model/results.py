423e10fcf04031b4073d616681f3c6bac318feda
Jan Medlock <jan.medlock@oregonstate.edu>
2017-02-08 18:53:04 -0800

    Make dumping & loading work.

--- model/results.py
+++ model/results.py
@@ -6,8 +6,10 @@ import os
 
 import joblib
 
+from . import multicountry
 from . import ODEs
 from . import parameters
+from . import regions
 from . import simulation
 
 
@@ -15,38 +17,52 @@ resultsdir = os.path.normpath(os.path.join(os.path.dirname(__file__),
                                            '../sim_data'))
 
 
-def get_path(country, target, parameters_type = 'sample'):
+def get_path(place, target, parameters_type = 'sample'):
     if parameters_type == 'sample' :
         suffix = ''
     else:
         suffix = '-' + parameters_type
     filename = '{}{}.pkl'.format(str(target), suffix)
-    return os.path.join(resultsdir, country, filename)
+    return os.path.join(resultsdir, place, filename)
 
 
 def dump(obj, parameters_type = None, compress = False):
-    if parameters_type is None:
-        # Try to guess.
-        if isinstance(obj.parameters, parameters.Samples):
-            parameters_type = 'sample'
-        elif isinstance(obj.parameters, parameters.Mode):
-            parameters_type = 'mode'
-
-    path = get_path(obj.parameters.country, obj.target,
-                    parameters_type = parameters_type)
+    if isinstance(obj, multicountry.MultiCountry):
+        path = get_path(obj.region, obj.target)
+    else:
+        if parameters_type is None:
+            # Try to guess.
+            if isinstance(obj.parameters, parameters.Samples):
+                parameters_type = 'sample'
+            elif isinstance(obj.parameters, parameters.Mode):
+                parameters_type = 'mode'
+        path = get_path(obj.parameters.country, obj.target,
+                        parameters_type = parameters_type)
     if not os.path.exists(os.path.dirname(path)):
         os.mkdir(os.path.dirname(path))
     return joblib.dump(obj.state, path, compress = compress)
 
 
-def load(country, target, parameters_type = 'sample'):
-    path = get_path(country, target,
+def load(place, target, parameters_type = 'sample'):
+    path = get_path(place,
+                    target,
                     parameters_type = parameters_type)
     state = joblib.load(path, mmap_mode = 'r')
-    return simulation._from_state(country, target, state, parameters_type)
+    if place == 'Global':
+        return multicountry.Global._from_state(target,
+                                               state)
+    elif place in regions.all_:
+        return multicountry.MultiCountry._from_state(place,
+                                                     target,
+                                                     state)
+    else:
+        return simulation._from_state(place,
+                                      target,
+                                      state,
+                                      parameters_type)
 
 
-def exists(country, target, parameters_type = 'sample'):
-    path = get_path(obj.parameters.country, obj.target,
+def exists(place, target, parameters_type = 'sample'):
+    path = get_path(place, target,
                     parameters_type = parameters_type)
     return os.path.exists(path)
