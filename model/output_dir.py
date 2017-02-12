92cc88ba2aacf8ac6dc8dee33824b3c35a6318f3
Jan Medlock <jan.medlock@oregonstate.edu>
2017-02-12 13:03:47 -0500

    Fix pickling.

---
+++ model/output_dir.py
@@ -0,0 +1,9 @@
+'''
+Path for saving and loading simulation output data.
+'''
+
+import os.path
+
+
+output_dir = os.path.normpath(os.path.join(os.path.dirname(__file__),
+                                           '../sim_data'))
