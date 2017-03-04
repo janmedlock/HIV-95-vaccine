436389c77c0b41254587ae6172b407b9ec84ceca
Jan Medlock <jan.medlock@oregonstate.edu>
2017-03-03 18:10:09 -0800

    Add plots.

--- tests/sample.py
+++ tests/sample.py
@@ -6,6 +6,9 @@ Test :meth:`model.parameters.Parameters.sample`.
 
 import sys
 
+from matplotlib import pyplot
+import seaborn
+
 sys.path.append('..')
 import model
 
@@ -14,11 +17,11 @@ if __name__ == '__main__':
     country = 'South Africa'
     nsamples = 2
 
-    print(country)
-
     parameters = model.parameters.Parameters(country)
     target = model.target.UNAIDS90()
-
     samples = parameters.sample(nsamples)
-
     multisim = model.simulation.MultiSim(samples, target)
+
+    pyplot.plot(model.simulation.t, multisim.new_infections.T)
+    pyplot.ylabel('New infections')
+    pyplot.show()
